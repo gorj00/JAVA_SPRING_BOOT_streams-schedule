5b7f36c7afaeb2e0471fcff74560488114b4f9b8
Jan Gorol <os.jangorol@gmail.com>
2023-01-09 20:28:03 +0100

    init

---
+++ src/main/java/dev/gorj00/streamsschedule/JavaSpringBootStreamsScheduleApplication.java
@@ -0,0 +1,14 @@
+package dev.gorj00.streamsschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootStreamsScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBootStreamsScheduleApplication.class,
+                args);
+    }
+
+}
---
+++ src/test/java/dev/gorj00/streamsschedule/JavaSpringBootStreamsScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gorj00.streamsschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootStreamsScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
